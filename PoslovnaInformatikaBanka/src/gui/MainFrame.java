a642df7c9e51a9850d220712ef9e3235048fc3df
Marko Micic <chakmi1@gmail.com>
2016-06-10 19:26:11 +0200

    sredjen import paketa

--- PoslovnaInformatikaBanka/src/gui/MainFrame.java
+++ PoslovnaInformatikaBanka/src/gui/MainFrame.java
@@ -3,12 +3,9 @@ package gui;
 import gui.listeners.MyWindowListener;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
 import java.sql.SQLException;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import users.User;
 import users.Users;
