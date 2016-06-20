6e2c924136788c845bb6d6dcc5c74f8437267177
Marko Micic <chakmi1@gmail.com>
2016-06-20 14:25:29 +0200

    izmenjen Menu klasa

--- PoslovnaInformatikaBanka/src/gui/Menu.java
+++ PoslovnaInformatikaBanka/src/gui/Menu.java
@@ -1,8 +1,36 @@
 package gui;
 
+import gui.actions.ImportNalogZaPlacanjeAction;
+import gui.dialogs.LoginDialog;
+import gui.dialogs.generic.AnalitikaIzvodaDialog;
+import gui.dialogs.generic.AnalitikaPresekaDialog;
+import gui.dialogs.generic.BankaDialog;
+import gui.dialogs.generic.DnevnoStanjeRacunaDialog;
+import gui.dialogs.generic.DrzavaDialog;
+import gui.dialogs.generic.KursUValutiDialog;
+import gui.dialogs.generic.KursnaListaDialog;
+import gui.dialogs.generic.MedjubankarskiNalogDialog;
+import gui.dialogs.generic.PoslovnoLiceDialog;
+import gui.dialogs.generic.PrenosIzvodaPresekDialog;
+import gui.dialogs.generic.RacunPoslovnihLicaDialog;
+import gui.dialogs.generic.UkidanjeDialog;
+import gui.dialogs.generic.ValutaDialog;
+import gui.dialogs.generic.VezaMedjubankarskogNalogaIStavkiDialog;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import javax.swing.Action;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+import db.DBConnection;
 
 @SuppressWarnings("serial")
 public class Menu extends JMenuBar {
@@ -27,6 +55,7 @@ public class Menu extends JMenuBar {
 	private JMenuItem itemMedjubankarskiNalog = new JMenuItem("Međubankarski nalog");
 	private JMenuItem itemVezaMedjubankarskogNalogaIStavke = new JMenuItem("Veza međubankarskog naloga i stavki");
 	private JMenuItem itemOdjaviSe=new JMenuItem("Odjavi se");
+	private Action actionImportNalog = new ImportNalogZaPlacanjeAction();
 	
 	public Menu() {
 		super();
@@ -45,12 +74,176 @@ public class Menu extends JMenuBar {
 		mnuBanka.add(itemAnalitikaIzvoda);
 		mnuBanka.add(itemMedjubankarskiNalog);
 		mnuBanka.add(itemVezaMedjubankarskogNalogaIStavke);
+		mnuOperacije.add(actionImportNalog);
 		mnuIzvestaji.add(itemSpisakRacuna);
 		mnuKorisnik.add(itemOdjaviSe);
 		add(mnuBanka);
 		add(mnuOperacije);
 		add(mnuIzvestaji);
 		add(mnuKorisnik);
+		
+		itemDrzave.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				DrzavaDialog dialog = new DrzavaDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+				
+			}
+			
+			
+		});
+		
+		itemValuta.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				ValutaDialog dialog = new ValutaDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+			}
+		});
+		
+		itemBanka.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				BankaDialog dialog = new BankaDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+			}
+		});
+		
+		itemKursnaLista.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				KursnaListaDialog dialog = new KursnaListaDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+			}
+		});
+		
+		itemKursUValuti.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				KursUValutiDialog dialog = new KursUValutiDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+			}
+		});
+		
+		itemPoslovnoLice.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				PoslovnoLiceDialog dialog=new PoslovnoLiceDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+				
+			}
+		});
+		
+		itemRacunPoslovnihLica.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				RacunPoslovnihLicaDialog dialog = new RacunPoslovnihLicaDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+			}
+		});
+		
+		itemUkidanje.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				UkidanjeDialog dialog = new UkidanjeDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+			}
+		});
+		
+		itemPrenosIzvodaPresek.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				PrenosIzvodaPresekDialog dialog = new PrenosIzvodaPresekDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+			}
+		});
+		
+		itemDnevnoStanjeRacuna.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				DnevnoStanjeRacunaDialog dialog = new DnevnoStanjeRacunaDialog(MainFrame.getInstance());
+				
+				dialog.setVisible(true);
+			}
+		});
+			
+		itemAnalitikaPreseka.addActionListener(new ActionListener() {
+				
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				AnalitikaPresekaDialog dialog = new AnalitikaPresekaDialog(MainFrame.getInstance());
+				
+				dialog.setVisible(true);
+			}
+		});
+			
+		itemAnalitikaIzvoda.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				AnalitikaIzvodaDialog dialog = new AnalitikaIzvodaDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+			}
+		});
+			
+		itemMedjubankarskiNalog.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				MedjubankarskiNalogDialog dialog = new MedjubankarskiNalogDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+			}
+		});
+		
+		itemVezaMedjubankarskogNalogaIStavke.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				VezaMedjubankarskogNalogaIStavkiDialog dialog = new VezaMedjubankarskogNalogaIStavkiDialog(MainFrame.getInstance());
+				dialog.setVisible(true);
+			}
+		});
+		
+		itemOdjaviSe.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				MainFrame.getInstance().setCurrentUser(null);
+				mnuKorisnik.setText("Anonimus");
+				LoginDialog dialog=new LoginDialog();
+				dialog.setVisible(true);
+			}
+		});
+		
+		itemSpisakRacuna.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+			          //System.out.println(getClass().getResource("Izvestaji/SpisakRacuna.jasper"));
+			          InputStream inputStream = new FileInputStream("Izvestaji/SpisakRacuna.jasper");
+			          JasperPrint jp = JasperFillManager.fillReport(inputStream,
+			          null, DBConnection.getConnection());
+			          JasperViewer jpv = new JasperViewer(jp,false);
+			          //JasperViewer.viewReport(jp, false);
+			          jpv.setSize(1000, 800);
+			          jpv.setVisible(true);
+			        } catch (Exception ex) {
+			          ex.printStackTrace();
+			        }
+			}
+		});
 		
 	}
 
---
+++ PoslovnaInformatikaBanka/src/gui/actions/ImportNalogZaPlacanjeAction.java
@@ -0,0 +1,75 @@
+package gui.actions;
+
+import exceptions.InvalidNalogZaPlacanjeException;
+import gui.MainFrame;
+import gui.tasks.ImportNalogZaPlacanje;
+
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+import javax.swing.AbstractAction;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class ImportNalogZaPlacanjeAction extends AbstractAction {
+
+	private JFileChooser fileChooser;
+	
+	public ImportNalogZaPlacanjeAction() {
+
+		putValue(NAME,"Import naloga za placanje");
+		File defaultLocation = new File("./testPrimeri");
+		fileChooser = new JFileChooser(defaultLocation);
+		fileChooser.setMultiSelectionEnabled(false);
+		FileNameExtensionFilter jksFilter = new FileNameExtensionFilter("XML file", "xml");
+		fileChooser.addChoosableFileFilter(jksFilter);
+		fileChooser.setFileFilter(jksFilter);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(fileChooser.showOpenDialog(MainFrame.getInstance()) == JFileChooser.APPROVE_OPTION){
+			MainFrame.getInstance().getContentPane().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+			File xmlFile = fileChooser.getSelectedFile();
+			String fileName = xmlFile.getName();
+			String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+			if(!extension.equalsIgnoreCase("xml"))
+				JOptionPane.showMessageDialog(MainFrame.getInstance(), "Nije izabran fajl sa .xml ekstenzijom", 
+						"Nepravilna ekstenzija", JOptionPane.ERROR_MESSAGE);
+			else{
+				try {
+					String importResponse = ImportNalogZaPlacanje.importNalog(new FileInputStream(xmlFile));
+					JOptionPane.showMessageDialog(MainFrame.getInstance(), "Izveštaj operacije importa: \n" +
+							importResponse,
+							"Izveštaj", JOptionPane.INFORMATION_MESSAGE);
+				} catch (FileNotFoundException e1) {
+					e1.printStackTrace();
+				} catch (SQLException e1) {
+					JOptionPane.showMessageDialog(MainFrame.getInstance(), 
+							"Desila se neo�?ekivana greška sa bazom podataka, molimo vas da kontaktirate administratora sistema: \n" +
+								e1.getMessage(), 
+							"Greška sa bazom podataka", JOptionPane.ERROR_MESSAGE);
+				} catch (InvalidNalogZaPlacanjeException e1) {
+					JOptionPane.showMessageDialog(MainFrame.getInstance(), 
+							"Nalog za plaćanje sadrži grešku zbog koje ne može nastaviti dalje, molimo vas da proverite nalog: \n" +
+								e1.getMessage(), 
+							"Nevalidan nalog plaćanja", JOptionPane.ERROR_MESSAGE);
+				} catch(Exception e1) {
+					e1.printStackTrace();
+					JOptionPane.showMessageDialog(MainFrame.getInstance(), 
+							"Exception has occured: \n" +
+								e1.getMessage(), 
+							"Exception", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			MainFrame.getInstance().getContentPane().setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+
+		}
+	}
+
+}
